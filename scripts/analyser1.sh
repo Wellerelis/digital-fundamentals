2aa39010f78329644ce265c97b6eb30c29185e3d
Konstantinova Margarita <margaritak821@gmail.com>
2025-11-16 21:20:32 +0300

    Remove extra newline in analyser1.sh

--- scripts/analyser1.sh
+++ scripts/analyser1.sh
@@ -6,7 +6,6 @@ do
      name=$(echo $info | cut -d ":" -f1)
      age=$(echo $info | cut -d ":" -f2)
      email=$(echo $info | cut -d ":" -f3)
-
      echo "name: " $name, "age: " $age, "email: " $email
   done
 done
